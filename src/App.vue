489132adeeb494f3082ddf241c15d04392223675
Kleuber Aguiar Da Silva <kleuber20@hotmail.com>
2021-08-01 10:39:20 -0300

    Add files clone Instagram

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <pagina-home></pagina-home>
+  </div>
+</template>
+
+<script>
+import Home from './views/Home.vue';
+export default {
+  name: 'app',
+  components:{
+    'pagina-home': Home
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="container">
+		<div class="cellphones">
+			<img src="../assets/images/gif-instagram.gif" class="gifInstagram" alt="Gif instagram">
+		</div>
+		<div class="instagramContainer">
+			<div class="instagramContainerStyle">
+				<img src="../assets/images/logo_instagram.svg" class="logo" alt="">
+				<form action="" class="formInstagram">
+					<input type="text" placeholder="User or email" class="texto">
+					<input type="password" placeholder="password" class="password">
+					<button class="enter">Entrar</button>
+					<h3 class="ou"><hr size="50" class="hrEsquerda">OU<hr size="50" class="hrDireita"></h3>
+					<h3 class="facebook"><ion-icon class="icone" name="logo-facebook"></ion-icon>Entrar com o facebook</h3>
+					<p class="changePassword">Esqueceu a senha?</p>
+				</form>
+			</div>
+			<div class="cadastro">
+				<h3 class="cadastrar">Não tem uma conta? <span class="cadastre">Cadastre-se</span></h3>
+			</div>
+			<span class="obterAplicativo">Obtenha o aplicativo.</span>
+			<div class="aplicativos">
+				<img src="../assets/images/btn_apple.png" alt="Apple">
+				<img src="../assets/images/btn_google.png" alt="Google">
+			</div>
+		</div>
+	</div>
+</template>
+<style src="../css/style.css"></style>
